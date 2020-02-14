3f2a854e45f138520a6055f313e96b37de739699
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 05:03:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,10 @@ def line(array)
   if(array.size==0)
     puts "The line is currently empty."
   else
-    "The line is currently:"
+    puts "The line is currently:"
     while(index < array.size) do
       index+=1
-      " #{index}. #{array[index]}"
+    puts " #{index}. #{array[index]}"
       
     end
   end
