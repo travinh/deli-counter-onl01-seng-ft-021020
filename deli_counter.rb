edc46a409ee024ac035d7961bdeb9ecabd72bc22
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 05:28:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  if(array==0)
+  if(array.size==0)
     puts "There is nobody waiting to be served!"
   else 
     puts "Currently serving #{array[0]}."
